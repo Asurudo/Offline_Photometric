a15f3f0c8f69ddd4035aefbda1a58be0d30369a5
Asurudo Jyo <asurudo@126.com>
2024-07-23 12:08:32 +0900

    基本实现光度学灯光

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ const double PI = 3.141592653;
 #include "stb_image.h"
 #include "texture.h"
 #include "transformation.h"
+#include "tiny_ldt.h"
 
 using namespace std;
 
@@ -45,37 +46,55 @@ vec3 randomInUnitDisk() {
   return p;
 }
 
+tiny_ldt<float>::light ldt;
+vector<vector<float>> intensityDis;
+
+float getIntesiy(float C, float gamma){
+  assert(C>=0 && C<=2*M_PI && gamma>=0 && gamma<=M_PI);
+  int Cindex = floor(C/M_PI*180.0/ldt.dc);
+  int gammaindex = floor(gamma/M_PI*180.0/ldt.dg);
+  if(gamma==0.0 || gamma==M_PI)
+    return intensityDis[Cindex][gammaindex];
+
+
+  float d = 0.0, e = 0.0;
+  while(d+ldt.dg<=gamma/M_PI*180.0)
+    d += ldt.dg;
+  while(e+ldt.dc<=C/M_PI*180.0)
+    e += ldt.dc;
+  float a = 1.0-(gamma/M_PI*180.0-d)/ldt.dg;
+  float b = 1.0-(C/M_PI*180.0-e)/ldt.dc;
+  float value1 = (a*intensityDis[Cindex][gammaindex]+(1-a)*intensityDis[Cindex][gammaindex+1]);
+  float value2 = (a*intensityDis[Cindex+1][gammaindex]+(1-a)*intensityDis[Cindex+1][gammaindex+1]);
+  return b*value1 + (1-b)*value2;
+}
+
 // 颜色着色
 vec3 color(const ray& in, int depth) {
   hit_record rec;
   // 减少误差，-0.00001也可以是交点
-  if (world.hitanythingbvh(in, 0.001, DBL_MAX, rec)) {
+  if (world.hitanything(in, 0.001, DBL_MAX, rec)) {
     // 反射出来的光线
     ray scattered;
     // 材料的吸收度
     vec3 attenuation;
     vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
-    if (depth < 50 && rec.mat_ptr->scatter(in, rec, attenuation, scattered))
+    if (depth < 5 && rec.mat_ptr->scatter(in, rec, attenuation, scattered))
       return emitted + attenuation * color(scattered, depth + 1);
     else {
       // 直视光源则可以看到光源原本的颜色
       if (!depth) emitted.make_unit_vector();
-      return emitted;
+      vec3 v = unit_vector(-in.direction());
+      return emitted*getIntesiy(atan2(-v.y(), -v.z()) + M_PI, M_PI - acos(-v.x()))/abs(dot(unit_vector(-in.direction()), unit_vector(vec3(-1, 0, 0))))/(80 - 30) / (350 - 300);
     }
   } else {
     return vec3(0, 0, 0);
-    // 天空
-    // // 将射线in单位化，让其长度为1
-    // vec3 unit_direction = unit_vector(in.direction());
-    // // 单位化后的射线的y的取值范围为[-1,1]，将其+1再除以2转化为[0,1]
-    // double t = 0.5 * (unit_direction.y() + 1.0);
-    // // 进行颜色插值
-    // return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
   }
   exit(0);
 }
 std::vector<shared_ptr<hitable>> worldlist;
 void buildWorld() {
+  texture* whitelightptr = new constant_texture(vec3(500, 500, 500));
   texture* mikulightptr = new constant_texture(vec3(0.223, 0.773, 0.733) * 15);
   texture* mikuptr = new constant_texture(vec3(0.223, 0.773, 0.733));
   texture* redptr = new constant_texture(vec3(0.65, 0.05, 0.05));
@@ -96,127 +115,20 @@ void buildWorld() {
                           jyorandengine.jyoRandGetReal<double>(0, 1))));
   texture* noisetextptr = new noise_texture(0.01);
 
-  int nx, ny, nn;
-  unsigned char* tex_data = stbi_load("earthmap.jpg", &nx, &ny, &nn, 0);
-  texture* imagetextureptr = new image_texture(tex_data, nx, ny);
-
-  // 地板
-  int nb = 20;
-  for (int i = 0; i < nb; i++)
-    for (int j = 0; j < nb; j++) {
-      double w = 100;
-      double x0 = -1000 + i * w;
-      double y0 = 0;
-      double z0 = -1000 + j * w;
-      double x1 = x0 + w;
-      double y1 = 100 * (jyorandengine.jyoRandGetReal<double>(0, 1) + 0.01);
-      double z1 = z0 + w;
-      worldlist.emplace_back(new box(vec3(x0, y0, z0), vec3(x1, y1, z1),
-                                     new lambertian(groundtexptr)));
-    }
   // 灯
-  worldlist.emplace_back(new rectangle_xz(123, 423, 147, 412, 554,
-                                          new diffuse_light(mikulightptr)));
-
-  // 运动的球
-  vec3 center(400, 400, 200);
-  worldlist.emplace_back(new moving_sphere(
-      center, center + vec3(50, 0, 0), 0.0, 10.0, 50.0,
-      new lambertian(new constant_texture(vec3(0.7, 0.3, 0.1)))));
-
-  // 玻璃球
+  worldlist.emplace_back(new rectangle_yz(30, 80, 300, 350, 500,
+                                         new diffuse_light(whitelightptr)));
   worldlist.emplace_back(
-      new sphere(vec3(260, 150, 45), 50, new dielectric(1.5)));
-
-  // 金属球
-  worldlist.emplace_back(
-      new sphere(vec3(0, 150, 145), 50,
-                 new metal(new constant_texture(vec3(0.8, 0.8, 0.9)), 10.0)));
+     new rectangle_xz(-1000, 1000, -1000, 1000, 0, new lambertian(whiteptr)));
 
   // 一个玻璃球与一团玻璃球形状的烟雾
-  hitable* glasssphereptr =
-      new sphere(vec3(360, 150, 145), 70, new dielectric(1.5));
-  worldlist.emplace_back(glasssphereptr);
-  worldlist.emplace_back(new smoke(glasssphereptr, 0.2,
-                                   new constant_texture(vec3(0.2, 0.4, 0.9))));
+  // hitable* glasssphereptr =
+  //     new sphere(vec3(360, 150, 145), 70, new dielectric(1.5));
+  // worldlist.emplace_back(glasssphereptr);
+  // worldlist.emplace_back(new smoke(glasssphereptr, 0.2,
+  //                                  new constant_texture(vec3(0.2, 0.4, 0.9))));
 
-  // 笼罩全图的战争迷雾
-  worldlist.emplace_back(
-      new smoke(new sphere(vec3(0, 0, 0), 5000, new dielectric(1.5)), 0.000005,
-                new constant_texture(vec3(1.0, 1.0, 1.0))));
-
-  // 地球
-  worldlist.emplace_back(
-      new sphere(vec3(400, 200, 400), 100, new lambertian(imagetextureptr)));
-
-  // 大理石球
-  worldlist.emplace_back(
-      new sphere(vec3(220, 280, 300), 80, new lambertian(noisetextptr)));
-
-  // 金属小球组成的立方体
-  int ns = 1000;
-  for (int j = 0; j < ns; j++)
-    worldlist.emplace_back(new translate(
-        new rotate_y(
-            new sphere(vec3(165 * jyorandengine.jyoRandGetReal<double>(0, 1),
-                            165 * jyorandengine.jyoRandGetReal<double>(0, 1),
-                            165 * jyorandengine.jyoRandGetReal<double>(0, 1)),
-                       10, new metal(whiteptr)),
-            15),
-        vec3(-100, 270, 395)));
-
-  // worldlist.emplace_back(
-  //     new sphere(vec3(0, -1000, 0), 1000, new lambertian(noisetextptr)));
-  // for (int a = -11; a < 11; a++) {
-  //   for (int b = -11; b < 11; b++) {
-  //     double choose_mat = jyorandengine.jyoRandGetReal<double>(0, 1);
-  //     vec3 center(a + 0.9 * jyorandengine.jyoRandGetReal<double>(0, 1),
-  //     0.2,
-  //                 b + 0.9 * jyorandengine.jyoRandGetReal<double>(0, 1));
-  //     if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
-  //       if (choose_mat < 0.8) {
-  //         worldlist.emplace_back(new moving_sphere(
-  //             center, center + vec3(0, 0.5, 0), 0.0, 1.0, 0.2,
-  //             new lambertian(new constant_texture(
-  //                 vec3(jyorandengine.jyoRandGetReal<double>(0, 1) *
-  //                          jyorandengine.jyoRandGetReal<double>(0, 1),
-  //                      jyorandengine.jyoRandGetReal<double>(0, 1) *
-  //                          jyorandengine.jyoRandGetReal<double>(0, 1),
-  //                      jyorandengine.jyoRandGetReal<double>(0, 1) *
-  //                          jyorandengine.jyoRandGetReal<double>(0,
-  //                          1))))));
-  //       } else if (choose_mat < 0.95)
-  //         worldlist.emplace_back(new sphere(
-  //             center, 0.2,
-  //             new metal(metaltexture,
-  //                       0.5 * jyorandengine.jyoRandGetReal<double>(0, 1)
-  //                       *
-  //                           jyorandengine.jyoRandGetReal<double>(0,
-  //                           1))));
-  //       else
-  //         worldlist.emplace_back(new sphere(center, 0.2, new
-  //         dielectric(1.5)));
-  //     }
-  //   }
-  // }
-
-  // worldlist.emplace_back(
-  //     new sphere(vec3(0, 2, 0), 2, new lambertian(noisetextptr)));
-  // worldlist.emplace_back(
-  //     new rectangle_plane_xy(3, 5, 1, 3, -2, new
-  //     diffuse_light(constantextptr)));
-  // worldlist.emplace_back(
-  //     new sphere(vec3(-4, 1, 0), 1,
-  //                new lambertian(new constant_texture(vec3(0.4, 0.2,
-  //                0.1)))));
-  // worldlist.emplace_back(
-  //     new sphere(vec3(4, 1, 0), 1, new metal(metaltexture, 0)));
-
-  // 从世界列表中创建bvh树
-  shared_ptr<hitable> rootptr;
-  bvh_node(worldlist, rootptr);
-  world = hitable_list(rootptr);
-  // world = hitable_list(worldlist);
+  world = hitable_list(worldlist);
 }
 
 int getfileline() {
@@ -234,8 +146,50 @@ int getfileline() {
 }
 
 int main() {
+  std::string err;
+  std::string warn;
+  if (!tiny_ldt<float>::load_ldt("photometry\\SLOTLIGHT_42184612.LDT", err, warn, ldt)) {
+    cout << "failed" << endl;
+  }
+  if (!err.empty()) 
+    cout << err << endl;
+  if (!warn.empty())
+    cout << warn << endl;
+  
+  int cnt = 0;
+  cout << ldt.dc << endl;//15
+  cout << ldt.dg << endl;//5
+
+  for(float i = 0.0; i <= 360.0; i += ldt.dc){
+    int j = i;
+    // 105/15=7 259/37-1=6
+    if((i/ldt.dc)>ldt.luminous_intensity_distribution.size()/((int)(180.0/ldt.dg)+1)-1 &&
+    (int)((ldt.luminous_intensity_distribution.size()/((int)(180.0/ldt.dg)+1)-1)*ldt.dc))
+    // 105 %= (259/37-1)=6*15
+      j %= (int)((ldt.luminous_intensity_distribution.size()/((int)(180.0/ldt.dg)+1)-1)*ldt.dc);
+    else if((i/ldt.dc)>ldt.luminous_intensity_distribution.size()/((int)(180.0/ldt.dg)+1)-1)
+      j = 0;
+    if(i==270 && (int)((ldt.luminous_intensity_distribution.size()/((int)(180.0/ldt.dg)+1)-1)*ldt.dc>=90))
+      j = 90;
+    // cout << "i" << i << " j" << j << endl;
+    // cout << (int)(j/ldt.dc)*((int)(180.0/ldt.dg)+1) << endl;
+    // cout << (int)(j/ldt.dc)*((int)(180.0/ldt.dg)+1)+(int)(180.0/ldt.dg)+1 << endl;
+    intensityDis.emplace_back(
+        vector<float>(ldt.luminous_intensity_distribution.begin()+(int)(j/ldt.dc)*((int)(180.0/ldt.dg)+1)
+                    , ldt.luminous_intensity_distribution.begin()+(int)(j/ldt.dc)*((int)(180.0/ldt.dg)+1)+(int)(180.0/ldt.dg)+1)
+    );
+  }
+  for(auto v: intensityDis){
+    for(auto p: v)
+      cout << p << " ";
+    cout << endl;
+  }
+  cout << intensityDis.size() << endl;
+  system("pause");
+  
+  
   // 是否重新渲染
-  int startoveragain = 0;
+  int startoveragain = 1;
 
   int curline = getfileline();
 
@@ -246,14 +200,14 @@ int main() {
     mout.open("output.PPM", ios::app);
 
   // 画布的长
-  int nx = 1000;
+  int nx = 800;
   // 画布的宽
-  int ny = 500;
+  int ny = 400;
   // 画布某一点的采样数量
   int ns = 5000;
 
   buildWorld();
-  vec3 lookfrom(478, 278, -600), lookat(278, 278, 0);
+  vec3 lookfrom(499.99, 800, 325), lookat(500, 0, 324.999999);
   camera cam(lookfrom, lookat, 40, double(nx) / double(ny), 0.0, 10.0, 0.0,
              1.0);
 
@@ -288,8 +242,8 @@ int main() {
           // double vplus = -0.5 + resqrtns * ((double)dj + jyorandengine.jyoRandGetReal<double>(-1, 1));
 
           // 点(u,v)是点(i,j)的反离散化
-          double u = (double(i) + uplus) / double(nx);
-          double v = (double(j) + vplus) / double(ny);
+          double u = double(i) / double(nx);
+          double v = double(j) / double(ny);
 
           // 一条射向画布上点(u,v)的光线，注意(u,v)不是真实坐标而是在画布上的比例位置
           ray r = cam.get_ray(u, v);
